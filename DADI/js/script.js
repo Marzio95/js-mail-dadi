b8f6e41f4693c941e513569858c38797cc4d7dd0
Marzio95 <marziodellarocca14@gmail.com>
2022-02-24 18:31:24 +0100

    sistemo layout

--- DADI/js/script.js
+++ DADI/js/script.js
@@ -1,19 +1,13 @@
 
 let container = document.querySelector('.container');
 
-// let arrayNumeriDado = [1, 2, 3, 4, 5, 6];
-
-// let theRandomNumberUtente = Math.floor(Math.random() * 5) + 1;
-// console.log(theRandomNumberUtente);
-// container.innerHTML = `<div>Numero Utente: ${theRandomNumberUtente}</div>`;
+let theRandomNumberUtente = Math.floor(Math.random() * 5) + 1;
+console.log(theRandomNumberUtente);
+container.innerHTML += `<div>Numero Utente: ${theRandomNumberUtente}</div>`;
 
 let theRandomNumberComputer = Math.floor(Math.random() * 5) + 1;
 console.log(theRandomNumberComputer);
-container.innerHTML = `<div>Numero Computer: ${theRandomNumberComputer}</div>`;
-
-let theRandomNumberUtente = Math.floor(Math.random() * 5) + 1;
-console.log(theRandomNumberUtente);
-container.innerHTML = `<div>Numero Utente: ${theRandomNumberUtente}</div>`;
+container.innerHTML += `<div>Numero Computer: ${theRandomNumberComputer}</div>`;
 
 if (theRandomNumberUtente > theRandomNumberComputer) {
     document.writeln("L'Utente Vince")   
