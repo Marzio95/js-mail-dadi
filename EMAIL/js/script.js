99b95e3a42f71e9ab4bfa55acd9597319f5d911f
Marzio95 <marziodellarocca14@gmail.com>
2022-02-24 15:59:47 +0100

    creo l array

---
+++ EMAIL/js/script.js
@@ -0,0 +1,6 @@
+
+const containerOfHtml = document.querySelector('.container')
+console.log(containerOfHtml)
+
+var arrayEmail = ["marzio@email","marco@email","mario@email","maro@email","marzo@email","mazio@email","mazo@email","arzio@email","fazio@email", ]
+console.log(arrayEmail)
