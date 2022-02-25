d4e6b99a29e489a45e185392bc062ca47e4116aa
Marzio95 <marziodellarocca14@gmail.com>
2022-02-25 11:47:27 +0100

    inserisco l evento click

--- EMAIL/js/script.js
+++ EMAIL/js/script.js
@@ -4,18 +4,30 @@ const containerOfHtml = document.querySelector('.container');
 
 var arrayEmail = ["marzio@email", "marco@email", "mario@email", "maro@email", "marzo@email", "mazio@email", "mazo@email", "arzio@email", "fazio@email",];
 
+let inuputButton = document.getElementById('email_utente')
+// let emailUtente = prompt('Email Utente:');
 
-let emailUtente = prompt('Email Utente:');
-containerOfHtml.innerHTML = `<div>Email Utente: ${emailUtente}</div>`;
+// containerOfHtml.innerHTML += `<div>Email Utente: ${emailUtente}</div>`;
+const cercaButton = document.getElementById('cerca')
 
-let research = false;
+cercaButton.addEventListener('click', myFunzione);
 
-for (let numEmail = 0; numEmail < arrayEmail.length; numEmail++) {
-    if (arrayEmail[numEmail] == emailUtente) {
-        research = true;
+function myFunzione() {
+    
+    let research = false;
+    let emailUtente = inuputButton.value;
+    for (let numEmail = 0; numEmail < arrayEmail.length; numEmail++) {
+        if (arrayEmail[numEmail] == emailUtente) {
+            research = true;
+        }
     }
-} if (research == false) {
-    document.writeln("La tua email non è presente nell'elenco, quindi non puoi accedere");
-} else {
-    document.writeln('Con questa email puoi accedere');
+    if (research == false) {
+        containerOfHtml.innerHTML += `<div>La tua email non è presente nell'elenco, quindi non puoi accedere</div>`;
+        // document.writeln("La tua email non è presente nell'elenco, quindi non puoi accedere");
+    } else {
+        containerOfHtml.innerHTML += `<div>Con questa email puoi accedere</div>`;
+    }
+
+    emailUtente = ''
+
 }
