aea73504f0f3e0443336aeec31ada11b098a4b58
Marzio95 <marziodellarocca14@gmail.com>
2022-02-26 18:28:19 +0100

    inserisco tasto annulla da js

--- EMAIL/js/script.js
+++ EMAIL/js/script.js
@@ -1,21 +1,35 @@
-
 const containerOfHtml = document.querySelector('.container');
+containerOfHtml.style.background = 'yellow'
+containerOfHtml.style.height = '300px'
+containerOfHtml.style.display = 'flex'
+containerOfHtml.style.flexDirection = 'column'
+containerOfHtml.style.alignItems = 'center'
+containerOfHtml.style.justifyContent = 'center'
 
 
-var arrayEmail = ["marzio@email", "marco@email", "mario@email", "maro@email", "marzo@email", "mazio@email", "mazo@email", "arzio@email", "fazio@email",];
+let titolo = document.createElement('h1')
+containerOfHtml.prepend(titolo)
+titolo.innerHTML = 'Selettore Email'
+
+let buttonAnnulla = document.createElement('button')
+containerOfHtml.append(buttonAnnulla)
+buttonAnnulla.innerHTML = 'Annulla'
+
+
+
+let arrayEmail = ["marzio@email", "marco@email", "mario@email", "maro@email", "marzo@email", "mazio@email", "mazo@email", "arzio@email", "fazio@email",];
 
 let inuputButton = document.getElementById('email_utente')
-// let emailUtente = prompt('Email Utente:');
 
-// containerOfHtml.innerHTML += `<div>Email Utente: ${emailUtente}</div>`;
 const cercaButton = document.getElementById('cerca')
+cercaButton.style.margin = '0.5rem'
 
-cercaButton.addEventListener('click', myFunzione);
-
-function myFunzione() {
+cercaButton.addEventListener('click', funzioneVerificaEmail);
+function funzioneVerificaEmail() {
     
     let research = false;
     let emailUtente = inuputButton.value;
+
     for (let numEmail = 0; numEmail < arrayEmail.length; numEmail++) {
         if (arrayEmail[numEmail] == emailUtente) {
             research = true;
@@ -23,11 +37,18 @@ function myFunzione() {
     }
     if (research == false) {
         containerOfHtml.innerHTML += `<div>La tua email non è presente nell'elenco, quindi non puoi accedere</div>`;
-        // document.writeln("La tua email non è presente nell'elenco, quindi non puoi accedere");
+      
     } else {
         containerOfHtml.innerHTML += `<div>Con questa email puoi accedere</div>`;
     }
 
-    emailUtente = ''
-
 }
+
+buttonAnnulla.addEventListener('click', funzioneAnnullaRicera);
+function funzioneAnnullaRicera(){
+    containerOfHtml.style.background = 'red'
+    
+      
+}
+
+
